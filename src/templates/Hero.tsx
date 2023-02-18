21f3faf29ff632fd28e2f5497f6d9f4e33b1af35
unknown <ffunes90@gmail.com>
2023-02-17 23:00:28 -0300

    refactor Hero

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -4,31 +4,11 @@ import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { Logo } from './Logo';
+import { Navbar } from './Navbar';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://www.facebook.com/marketplace/profile/100009479834691">
-            <a target="_blank">Tienda</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>Nosotros</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contacto</a>
-          </Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
-
+  <Background color="bg-gray-100"> 
+    <Navbar></Navbar>
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
         title={
--- src/templates/Store.tsx
+++ src/templates/Store.tsx
@@ -2,10 +2,12 @@ import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
+import { Navbar } from './Navbar';
 
 const Store = () => (
   <div className="antialiased text-gray-600">
     <Meta title={AppConfig.title} description={AppConfig.description} />
+    <Navbar/>
     <Banner />
     <Footer />
   </div>
